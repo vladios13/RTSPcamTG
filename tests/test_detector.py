d4a4ab4130497b0755e88a9ba14732deedc70e79
vladios13 <vladios13@yandex.com>
2026-03-23 20:04:00 +0300

    Чистка

--- tests/test_detector.py
+++
@@ -1,8 +0,0 @@
-import cv2
-from app import detector
-
-# Тест разницы между кадрами
-img1 = cv2.imread('1.jpg', 0)
-img2 = cv2.imread('2.jpg', 0)
-diff = detector.get_image_difference(img1, img2)
-print('Image difference:', diff)
